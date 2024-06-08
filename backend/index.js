f93a3aafae42e60f96d9918133c2175dc978b62e
Roshan Kumar <rsahu7989@gmail.com>
2024-06-08 16:15:29 +0530

    chore: Update npm dependencies and remove commented out code

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import ratingRoute from './routes/rating.route.js'
 import bookingRoute from './routes/booking.route.js'
 import cookieParser from 'cookie-parser'
 import path from 'path'
+const PORT = process.env.PORT || 8000
 const app = express()
 
 const __dirname = path.resolve()
@@ -22,23 +23,27 @@ mongoose
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.get('/api', (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to Travel Tourism API",
+        PORT: process.env.PORT | "default 8000"
+    })
+})
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
 app.use('/api/package', packageRoute)
 app.use('/api/rating', ratingRoute)
 app.use('/api/booking', bookingRoute)
 
-// //rest api
-
 //static files
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+// })
 
 //port
-app.listen(8000, () => {
-    console.log('listening on 8000')
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}/api`)
 })
