aa9e5dfa3f6f53ab11af398a0eaa36621666be5a
Roshan Kumar <rsahu7989@gmail.com>
2024-06-09 09:35:03 +0530

    chore: Update CORS origin to include localhost for development

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:8000'}`;
+
+  return {
+    server: {
+      proxy: {
+        '/api': API_URL,
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
