8dbd80c27a019e348728b8be08a1fb9528637736
Roshan Kumar <rsahu7989@gmail.com>
2024-06-10 19:22:49 +0530

    removed unneccesary imports

--- client/src/pages/admin/AllUsers.jsx
+++ client/src/pages/admin/AllUsers.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { deleteUser, getAllUsers } from "../../http";
 
